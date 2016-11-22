4f18e3ebdbf370b0a4ce433be3180fb55732b771
lauramorfin <laura-morfin@my.codergv.org>
2016-11-22 12:09:19 -0600

    heroku changed port

--- app.js
+++ app.js
@@ -3,8 +3,16 @@ var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
+app.set('port', (process.env.PORT || 5000));
+
 app.use(express.static(__dirname+'/client'));
 app.use(bodyParser.json());
+
+
+
+
+
+
 
 Genre =require('./models/genre');
 Book =require('./models/book');
@@ -106,5 +114,9 @@ app.delete('/api/books/:_id', function(req, res){
 	});
 });
 
-app.listen(3000);
-console.log('Running on port 3000...');
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+} || 3000);
+
+// app.listen(process.env.PORT || 3000);
+// console.log('Running on port 3000...');
